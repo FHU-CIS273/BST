38757286ce14fd30e823be361087063741b85ba8
Kenan Casey <kenancasey@gmail.com>
2019-10-30 09:40:48 -0500

    initial commit

---
+++ BST/BinarySearchTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace BST
+{
+
+    public class Node<T>
+    {
+        int Key { get; set; }
+        T Data { get; set; }
+
+        Node<T> Left { get; set; }
+        Node<T> Right { get; set; }
+
+        public Node(int key)
+        {
+            Key = key;
+            Left = Right = null;
+        }
+
+    }
+
+    public class BinarySearchTree<T>
+    {
+        Node<T> root;
+
+        public BinarySearchTree()
+        {
+        }
+    }
+}
