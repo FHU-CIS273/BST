87c10c26a782e21fc60bbef435924631251462d1
Kenan Casey <kenancasey@gmail.com>
2019-10-31 16:06:07 -0500

    Add method stubs

--- BST/BinarySearchTree.cs
+++ BST/BinarySearchTree.cs
@@ -1,18 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace BST
 {
 
     public class Node<T>
     {
-        int Key { get; set; }
-        T Data { get; set; }
+        public int Key { get; set; }
+        public T Data { get; set; }
 
-        Node<T> Left { get; set; }
-        Node<T> Right { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
 
-        public Node(int key)
+        public Node(int key, T data)
         {
             Key = key;
+            Data = data;
             Left = Right = null;
         }
 
@@ -20,10 +23,144 @@ namespace BST
 
     public class BinarySearchTree<T>
     {
-        Node<T> root;
+        Node<T> root = null;
 
         public BinarySearchTree()
         {
         }
+
+        public Node<T> Search(int key)
+        {
+            return SearchRecursive(root, key);
+        }
+
+        private Node<T> SearchRecursive(Node<T> node, int key)
+        {
+            if (node != null)
+            {
+                if (key == node.Key)
+                {
+                    return node;
+                }
+                else if (key < node.Key)
+                {
+                    return SearchRecursive(node.Left, key);
+                }
+                else
+                {
+                    return SearchRecursive(node.Right, key);
+                }
+            }
+
+            return null;
+        }
+
+        public void Insert(int key, T data)
+        {
+            var newNode = new Node<T>(key, data);
+
+            if (root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                InsertRecursive(root, newNode);
+            }
+        }
+
+        // TODO 
+        private void InsertRecursive(Node<T> parent, Node<T> newNode)
+        {
+            
+        }
+
+        // TODO
+        public bool Remove(int key)
+        {
+            return true;
+        }
+
+        // TODO
+        private bool RemoveRecursive(Node<T> node, int key)
+        {
+            return true;
+        }
+
+        // TODO
+        public int Height
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public Node<T> Parent(Node<T> node)
+        {
+            return ParentRecursive(root, node);
+        }
+
+        // TODO
+        private Node<T> ParentRecursive(Node<T> subtreeRoot, Node<T> node)
+        {
+            return null;
+        }
+
+
+        // TODO
+        public List<int> InOrderKeys
+        {
+            get
+            {
+                List<int> keys = new List<int>();
+                InOrderKeysRecursive(root, keys);
+                return keys;
+
+            }
+        }
+
+        // TODO
+        private void InOrderKeysRecursive(Node<T> node, List<int> list)
+        {
+            
+        }
+
+
+        
+        public List<int> PreOrderKeys
+        {
+            get
+            {
+                List<int> keys = new List<int>();
+                PreOrderKeysRecursive(root, keys);
+                return keys;
+            }
+        }
+
+        // TODO
+        private void PreOrderKeysRecursive(Node<T> node, List<int> list)
+        {
+            
+        }
+
+        
+        public List<int> PostOrderKeys
+        {
+            get
+            {
+                List<int> keys = new List<int>();
+                PostOrderKeysRecursive(root, keys);
+                return keys;
+            }
+        }
+        // TODO
+        private void PostOrderKeysRecursive(Node<T> node, List<int> list)
+        {
+          
+        }
+
+
+
     }
 }
